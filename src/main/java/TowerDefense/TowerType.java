32a72e4d40a22a2e69a4e78b39e740737a2d69d1
vuthaouet <55568077+vuthaouet@users.noreply.github.com>
2019-10-29 14:54:19 +0700

    update thêm

--- src/main/java/TowerDefense/TowerType.java
+++ src/main/java/TowerDefense/TowerType.java
@@ -6,7 +6,8 @@ import static Util.Artist.QuickLoad;
 
 public enum TowerType {
     CannonRed(new Texture[]{QuickLoad("CannonBase.png"), QuickLoad("CannonGun.png")}, 10, 300,0.5f),
-    CannonSniper(new Texture[]{QuickLoad("CannonSniper.png"),QuickLoad("CannonGun.png")}, 10,300,0.5f);
+    CannonSniper(new Texture[]{QuickLoad("Cannon.png"),QuickLoad("CannonGun.png")}, 10,300,3f),
+    Cannon(new Texture[]{QuickLoad("Cannon.png"),QuickLoad("CannonGun2.png")}, 10,300,3f);
     Texture[] textures;
     int damage, range;
     float firingSpeed;
